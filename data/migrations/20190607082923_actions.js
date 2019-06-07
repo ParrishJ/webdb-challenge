9514edd6087059d2509ae4ca904cab26eb8fb6af
ParrishJ <jarparrish@gmail.com>
2019-06-07 09:08:16 -0700

    db and tables migrated

--- data/migrations/20190607082923_actions.js
+++ data/migrations/20190607082923_actions.js
@@ -1,8 +1,30 @@
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('actions', function (tbl) {
+        tbl
+            .increments()
+
+        tbl
+            .string('actionDescription', 500)
+
+        tbl
+            .string('actionNotes', 500)
+
+        tbl
+            .boolean('actionCompleted')
+
+        tbl
+            .integer('project_id')
+            .unsigned()
+            .references('id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+
+    })
+
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('actions')
 };
